ea7b3318dd47d3205614d7d9767febdd56ee4ef7
David Brown <david.brown@pnnl.gov>
2016-04-05 10:16:18 -0700

    Download right software
    Going through the procedure for bringing up the cluster on a
    different system finding bugs and fixing them.

--- download-software.sh
+++ download-software.sh
@@ -3,10 +3,9 @@
 wget -O vagrant.rpm https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.rpm
 wget -O chefdk.rpm https://opscode-omnibus-packages.s3.amazonaws.com/el/7/x86_64/chefdk-0.11.0-1.el7.x86_64.rpm
 wget -O chef.rpm https://opscode-omnibus-packages.s3.amazonaws.com/el/7/x86_64/chef-12.7.2-1.el7.x86_64.rpm
-wget -O slurm.tar.bz2 http://www.schedmd.com/download/latest/slurm-15.08.8.tar.bz2
 
 echo "
 Chef Server requires a click through agreement so please go to the chef server
-url for RHEL 6 version 12.4.1 (https://downloads.chef.io/chef-server/redhat/)
+url for RHEL 7 version 12.4.1 (https://downloads.chef.io/chef-server/redhat/)
 and download the rpm and copy it to chef-server.rpm in this directory.
 "
